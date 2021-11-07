70cbb4894179351f1010a5fc59ab63b1d4549056
Jim Zucker <jim@zucker.us>
2021-11-07 08:58:23 -0800

    Update license and add sql for db

---
+++ METERING_HISTORY_TREND.sql
@@ -0,0 +1,83 @@
+/*
+Licensed to the Apache Software Foundation (ASF) under one
+or more contributor license agreements.  See the NOTICE file
+distributed with this work for additional information
+regarding copyright ownership.  The ASF licenses this file
+to you under the Apache License, Version 2.0 (the
+"License"); you may not use this file except in compliance
+with the License.  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing,
+software distributed under the License is distributed on an
+"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+KIND, either express or implied.  See the License for the
+specific language governing permissions and limitations
+under the License.
+*/
+
+CREATE DATABASE IF NOT EXISTS USAGE_MONITOR;
+USE DATABASE USAGE_MONITOR;
+CREATE OR REPLACE PROCEDURE CALC_METERING_HISTORY_TREND() 
+RETURNS VARCHAR
+LANGUAGE javascript
+as
+$$
+var who_cares = snowflake.execute( { sqlText:
+        `CREATE OR REPLACE TEMPORARY TABLE METERING_HISTORY_SUMMARY(START_TIME_MONTH int, CREDITS_USED double, FORECAST double );`
+       } );
+       
+var who_cares = snowflake.execute( { sqlText:
+        `INSERT INTO METERING_HISTORY_SUMMARY
+            SELECT Top 2 START_TIME_MONTH, CREDITS_USED, FORECAST FROM
+              (
+                SELECT MONTH(START_TIME) + YEAR(START_TIME)*100 START_TIME_MONTH,
+                    SUM(CREDITS_USED_COMPUTE) CREDITS_USED_COMPUTE,
+                    SUM(CREDITS_USED_CLOUD_SERVICES) CREDITS_USED_CLOUD_SERVICES,
+                    SUM(CREDITS_USED) CREDITS_USED,
+                    SUM(CREDITS_USED) + ( AVG(CREDITS_USED) * ( last_day(to_date(getdate())) - to_date(MAX(START_TIME)) ) )  FORECAST
+                FROM "SNOWFLAKE"."ACCOUNT_USAGE"."METERING_HISTORY"
+                WHERE MONTH(START_TIME) = MONTH(getdate())
+                GROUP BY 1
+                UNION ALL
+                SELECT MONTH(START_TIME) + YEAR(START_TIME)*100 START_TIME_MONTH,
+                    SUM(CREDITS_USED_COMPUTE) CREDITS_USED_COMPUTE,
+                    SUM(CREDITS_USED_CLOUD_SERVICES) CREDITS_USED_CLOUD_SERVICES,
+                    SUM(CREDITS_USED) CREDITS_USED,
+                    SUM(CREDITS_USED) FORECAST
+                FROM "SNOWFLAKE"."ACCOUNT_USAGE"."METERING_HISTORY"
+                WHERE MONTH(START_TIME) < MONTH(getdate())
+                GROUP BY 1
+                UNION ALL
+                    SELECT 10990101, 0.01, 0.01, 0.01, 0.1
+                )
+            ORDER BY 1;`
+       } );
+
+var who_cares = snowflake.execute( { sqlText:
+      `CREATE OR REPLACE TEMPORARY TABLE METERING_HISTORY_TREND(ACCOUNT VARCHAR(25), "MTD" VARCHAR(25), "FORECAST" VARCHAR(25), "PRIOR_MONTH" VARCHAR(25), "CHANGE" VARCHAR(25) );`
+       } );
+       
+var who_cares = snowflake.execute( { sqlText:
+      `INSERT INTO METERING_HISTORY_TREND
+       SELECT ACCOUNT, TO_CHAR(SUM(CREDITS_USED),'999,999.00') "MTD", 
+                TO_CHAR(SUM(FORECAST),'999,999.00') "FORECAST", 
+                TO_CHAR(SUM(PRIOR_MONTH),'999,999.00') "PRIOR_MONTH", 
+                TO_CHAR((SUM(FORECAST) - SUM(PRIOR_MONTH))/SUM(PRIOR_MONTH)*100, '999.0"%"') "CHANGE"
+        FROM (
+          SELECT CURRENT_ACCOUNT() as ACCOUNT, CREDITS_USED, 0 PRIOR_MONTH, 0 "CHANGE", FORECAST FROM METERING_HISTORY_SUMMARY
+          WHERE START_TIME_MONTH = (SELECT MAX(START_TIME_MONTH) FROM METERING_HISTORY_SUMMARY)
+          UNION ALL
+          SELECT CURRENT_ACCOUNT(), 0, CREDITS_USED, 0, 0 FROM METERING_HISTORY_SUMMARY
+          WHERE START_TIME_MONTH = (SELECT MIN(START_TIME_MONTH) FROM METERING_HISTORY_SUMMARY)
+        )
+        GROUP BY 1;`
+       } );
+
+  var who_cares = snowflake.execute( { sqlText:
+       `DROP TABLE METERING_HISTORY_SUMMARY`
+       } );
+
+  return 'Done.';
+$$;
--- README.md
+++ README.md
@@ -1,4 +1,50 @@
+# Snowflake Usage Monitoring
+## User Story
+As a Developer I want a daily usage update that shows how my compute and storage usage is trending to the prior month.
 
-#
-https://community.snowflake.com/s/article/How-to-run-AWS-Lambda-function-with-Snowflake-Python-Connector
+The goal is for this to be similiar to the data we get from AWS Cost Explorer.  
 
+As the Snowflake Views do not seem to have cost info we will show the data in capacity units that it is quoted in,
+The METERING_HISTORY for compute does not seem to have a forecast so we will create a simple one based on MTD average where 
+	FORECAST = MTD + RemainingDays in month * AVG(MTD Daily Usage)
+
+We will support 2 options in configutaration for output to slack
+
+### Format 1: Just one line for an Account
+
+Account | Forecast |  Trend
+12345   | 9,999.9  |  +/-XX%
+
+### Format 2: One row for each Warehouse
+<accountId>    | Forecast |  Trend
+WAREHOUSE1     | 9,999.9  |  +/-XX%
+WAREHOUSE2     | 9,999.9  |  +/-XX%
+WAREHOUSE3     | 9,999.9  |  +/-XX%
+
+
+## Acceptance Criteria
+1. Instructions provided in README to install
+2. Both formats work on Slack using Snowflake External Function Feature
+
+# Installation Instructions
+
+## Snowflake View Installation Iinstructions
+1. Run the sql `METERING_HISTORY_TREND.sql` to create the USAGE_MONITOR database and CALC_METERING_HISTORY_TREND stored procedure.
+2. Execute the stored procedure to populate the view METERING_HISTORY_TREND
+`USE DATABASE USAGE_MONITOR;
+call CALC_METERING_HISTORY_TREND();`
+3. Query the view to see the current statistics:
+`SELECT * from METERING_HISTORY_TREND;`
+
+
+## AWS Slack External Function Installation Instructions
+
+
+
+# References
+
+## USAGE view in SNOWFLAKE Datbase
+https://docs.snowflake.com/en/sql-reference/account-usage.html
+
+## how to create an external function
+https://docs.snowflake.com/en/sql-reference/external-functions-creating-aws-template.html
