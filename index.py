d5915887074a1330b797f8cfc7dfc908d0eb4d7b
Subash Chandra <subashc2023@gmail.com>
2023-02-17 19:52:42 -0600

    Update index.py

--- index.py
+++ index.py
@@ -1,8 +1,11 @@
+#To add more information from Snowflake, you must edit the External Function to send more columns, and then 
+# Then change the Header column to reflect the new Headers, as well as add the column to this line in handler.
+# default = database, forecast, change = row[1:]
+
 import requests
 import boto3
 import json
 import logging
-import datetime
 
 logging.basicConfig(level = logging.INFO)
 logger = logging.getLogger()
@@ -12,74 +15,58 @@ REGION = "us-east-1"
 SLACK_SECRET_KEY_NAME="slack_url"
 TEAMS_SECRET_KEY_NAME="teams_url"
 
-
 def get_secret(secret_name: str, region_name: str) -> str:
-	session = boto3.session.Session()
-	client = session.client("secretsmanager", region_name=region_name)
-	try:
-		secret = client.get_secret_value(SecretId=secret_name)["SecretString"]
-	except Exception as e:
-		if e.response['Error']['Code'] == 'InvalidRequestException':
-			logger.error("The request was invalid due to:", e)
-		elif e.response['Error']['Code'] == 'InvalidParameterException':
-			logger.error("The request had invalid params:", e)
-			
-	return secret
+    session = boto3.session.Session()
+    client = session.client("secretsmanager", region_name=region_name)
+    secret = client.get_secret_value(SecretId=secret_name)["SecretString"]
+    return secret
 
 def send_slack_message(slack_url: str, message: str):
-	if slack_url == "":
-		return
-	slack_message = {
-		'text': message
-	}
- 
-	response = requests.post(slack_url, json.dumps(slack_message).encode('utf-8'))
-	if response.status_code != 200:
-		print("Failed to post to Slack. Response:", response.text)
-	else:
-		print("Slack message posted successfully")
-	if response.status_code != 200:
-		print("Failed to post to Slack. Response:", response.text)
-	else:
-		print("Slack message posted successfully")
-	  
+    message = "```" + message + "```"
+    response = requests.post(slack_url, json={"text": message})
+    if response.status_code != 200:
+        print("Failed to post to Slack. Response:", response.text)
+    else:
+        print("Slack message posted successfully")
+        
 def send_teams_message(teams_url: str, message: str):
-	if teams_url == "":
-		return
-
-	teams_message = {
-		'text': message
-	}
-	response = requests.post(teams_url, json.dumps(teams_message).encode('utf-8'))
-	if response.status_code != 200:
-		print("Failed to post to Teams. Response:", response.text)
-	else:
-		print("Teams message posted successfully")
+    message = "```\n" + message + "\n```"
+    response = requests.post(teams_url, json={"text": message})
+    if response.status_code != 200:
+        print("Failed to post to Teams. Response:", response.text)
+    else:
+        print("Teams message posted successfully")
 
 def handler(event, context):
-	result = []
-	status_code = 200
-	try:
-		event_body = json.loads(event["body"])
-		rows = event_body["data"]
-		result.append("```\n")
-  
-		for row in rows:
-			database, forecast, change = row[1:]
-			database = database.ljust(16)
-			forecast = forecast.ljust(10)
-			change = change.strip(8)
-			result.append(f"{database} | {forecast} | ({change})\n")
-		result.append("```")
+    result = []
+    status_code = 200
+    try:
+        event_body = json.loads(event["body"])
+        rows = event_body["data"]
+        
+        account_width = 18
+        forecast_width = 10
+        
+        #Add Header row and Divider
+        header = "Account".ljust(account_width) + " | " + "Forecast".ljust(forecast_width) + " | " + "Change".strip()
+        result.append(header)
+        divider = "-" * len(header)
+        result.append(divider)
+        
+        for row in rows:
+            database, forecast, change = row[1:]
+            str_forecast = str(forecast).ljust(forecast_width)
+            result.append(f"{database.ljust(account_width)} | {str_forecast} | {change.strip()}")
+        
+        secret = get_secret(SECRET_MANAGER, REGION)
+        
+        slack_url = json.loads(secret)[SLACK_SECRET_KEY_NAME]
+        send_slack_message(slack_url, '\n'.join(result))
+        
+        teams_url = json.loads(secret)[TEAMS_SECRET_KEY_NAME]
+        send_teams_message(teams_url, '\n'.join(result))
 
-		secret = get_secret(SECRET_MANAGER, REGION)
-		
-		slack_url = json.loads(secret)[SLACK_SECRET_KEY_NAME]
-		send_slack_message(slack_url, '\n'.join(result))
-		
-		teams_url = json.loads(secret)[TEAMS_SECRET_KEY_NAME]
-		send_teams_message(teams_url, '\n'.join(result))
-	except Exception as err:
-		status_code = 400
+    except Exception as err:
+        status_code = 400
 
-	return {"statusCode": status_code}
+    return {"statusCode": status_code}
