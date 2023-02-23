7739ad3a4894aa529ec75e637508d82954b0797f
Subash Chandra <subashc2023@gmail.com>
2023-02-23 09:59:03 -0600

    Upgraded Python to 3.9, and replaced 'requests' with UrlLib,Request
    Replaced the 'requests' library for post requests with UrlLib.Request for compatibility with Python 3.9

--- index.py
+++ index.py
@@ -2,18 +2,19 @@
 # Then change the Header column to reflect the new Headers, as well as add the column to this line in handler.
 # default = database, forecast, change = row[1:]
 
-import requests
+import urllib.request
 import boto3
 import json
 import logging
 
-logging.basicConfig(level = logging.INFO)
+logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger()
 
-SECRET_MANAGER="snowflake_secret_manager"
+SECRET_MANAGER = "snowflake_secret_manager"
 REGION = "us-east-1"
-SLACK_SECRET_KEY_NAME="slack_url"
-TEAMS_SECRET_KEY_NAME="teams_url"
+SLACK_SECRET_KEY_NAME = "slack_url"
+TEAMS_SECRET_KEY_NAME = "teams_url"
+
 
 def get_secret(secret_name: str, region_name: str) -> str:
     session = boto3.session.Session()
@@ -21,21 +22,28 @@ def get_secret(secret_name: str, region_name: str) -> str:
     secret = client.get_secret_value(SecretId=secret_name)["SecretString"]
     return secret
 
+
 def send_slack_message(slack_url: str, message: str):
     message = "```" + message + "```"
-    response = requests.post(slack_url, json={"text": message})
-    if response.status_code != 200:
-        logger.error("Failed to post to Slack. Response: %s", response.text)
-    else:
-        logger.info("Slack message posted successfully")
+    data = json.dumps({"text": message}).encode("utf-8")
+    req = urllib.request.Request(slack_url, data=data, headers={"Content-Type": "application/json"})
+    with urllib.request.urlopen(req) as response:
+        if response.status == 200:
+            logger.info("Slack message posted successfully")
+        else:
+            logger.error("Failed to post to Slack. Response: %s", response.read().decode("utf-8"))
+
 
 def send_teams_message(teams_url: str, message: str):
     message = "```\n" + message + "\n```"
-    response = requests.post(teams_url, json={"text": message})
-    if response.status_code != 200:
-        logger.error("Failed to post to Teams. Response: %s", response.text)
-    else:
-        logger.info("Teams message posted successfully")
+    data = json.dumps({"text": message}).encode("utf-8")
+    req = urllib.request.Request(teams_url, data=data, headers={"Content-Type": "application/json"})
+    with urllib.request.urlopen(req) as response:
+        if response.status == 200:
+            logger.info("Teams message posted successfully")
+        else:
+            logger.error("Failed to post to Teams. Response: %s", response.read().decode("utf-8"))
+
 
 def handler(event, context):
     result = []
@@ -43,10 +51,10 @@ def handler(event, context):
     try:
         event_body = json.loads(event["body"])
         rows = event_body["data"]
-        
+
         account_width = 19
         forecast_width = 12
-        
+
         for idx, row in enumerate(rows):
             if idx == 0:
                 database, forecast, change = row[1:]
@@ -62,13 +70,13 @@ def handler(event, context):
                 forecast = str(forecast)
             str_forecast = str(forecast).ljust(forecast_width)
             result.append(f"{database.ljust(account_width)} | {str_forecast} | {change.strip()}")
-        
+
         secret = get_secret(SECRET_MANAGER, REGION)
         result.append("* Forecast components may be ±15-25%")
 
         slack_url = json.loads(secret)[SLACK_SECRET_KEY_NAME]
         send_slack_message(slack_url, '\n'.join(result))
-        
+
         teams_url = json.loads(secret)[TEAMS_SECRET_KEY_NAME]
         send_teams_message(teams_url, '\n'.join(result))
 
