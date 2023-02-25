f78d0055615afbbb4892bf4b765f18b547d5ac4c
Subash Chandra <120974338+subashc2023@users.noreply.github.com>
2023-02-24 22:54:53 -0600

    Add Teams Integration and converted forecast to dollars (#5)
    Add Teams Integration and converted forecast to dollars (#5)

--- index.py
+++ index.py
@@ -1,48 +1,86 @@
-import requests
+#To add more information from Snowflake, you must edit the External Function to send more columns, and then 
+# Then change the Header column to reflect the new Headers, as well as add the column to this line in handler.
+# default = database, forecast, change = row[1:]
+
+import urllib.request
 import boto3
 import json
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger()
+
+SECRET_MANAGER = "snowflake_secret_manager"
+REGION = "us-east-1"
+SLACK_SECRET_KEY_NAME = "slack_url"
+TEAMS_SECRET_KEY_NAME = "teams_url"
+
 
 def get_secret(secret_name: str, region_name: str) -> str:
-  session = boto3.session.Session()
-  client = session.client("secretsmanager", region_name=region_name)
-  secret = client.get_secret_value(SecretId=secret_name)["SecretString"]
-  return secret
+    session = boto3.session.Session()
+    client = session.client("secretsmanager", region_name=region_name)
+    secret = client.get_secret_value(SecretId=secret_name)["SecretString"]
+    return secret
+
 
 def send_slack_message(slack_url: str, message: str):
-  response = requests.post(slack_url, json={"text": message})
-  if response.status_code != 200:
-      print("Failed to post to Slack. Response:", response.text)
-  else:
-      print("Slack message posted successfully")
-      
+    message = "```" + message + "```"
+    data = json.dumps({"text": message}).encode("utf-8")
+    req = urllib.request.Request(slack_url, data=data, headers={"Content-Type": "application/json"})
+    with urllib.request.urlopen(req) as response:
+        if response.status == 200:
+            logger.info("Slack message posted successfully")
+        else:
+            logger.error("Failed to post to Slack. Response: %s", response.read().decode("utf-8"))
+
+
 def send_teams_message(teams_url: str, message: str):
-  response = requests.post(teams_url, json={"text": message})
-  if response.status_code != 200:
-      print("Failed to post to Teams. Response:", response.text)
-  else:
-      print("Teams message posted successfully")
-      
+    message = "```\n" + message + "\n```"
+    data = json.dumps({"text": message}).encode("utf-8")
+    req = urllib.request.Request(teams_url, data=data, headers={"Content-Type": "application/json"})
+    with urllib.request.urlopen(req) as response:
+        if response.status == 200:
+            logger.info("Teams message posted successfully")
+        else:
+            logger.error("Failed to post to Teams. Response: %s", response.read().decode("utf-8"))
+
 
 def handler(event, context):
-  result = []
-  status_code = 200
-  try:
-      event_body = json.loads(event["body"])
-      rows = event_body["data"]
+    result = []
+    status_code = 200
+    try:
+        event_body = json.loads(event["body"])
+        rows = event_body["data"]
 
-      for row in rows:
-          database, forecast, change = row[1:]
-          result.append(f"*{database}* - Forecast: {forecast} ({change.strip()})")
-      
-      secret = get_secret("slackurl", "us-east-1")
-      
-      slack_url = json.loads(secret)["slack_url"]
-      send_slack_message(slack_url, '\n'.join(result))
-      
-      teams_url = json.loads(secret)["teams_url"]
-      send_teams_message(teams_url, '\n'.join(result))
+        account_width = 19
+        forecast_width = 12
 
-  except Exception as err:
-      status_code = 400
+        for idx, row in enumerate(rows):
+            if idx == 0:
+                database, forecast, change = row[1:]
+                header = f"{'Account'.ljust(account_width)} | {'Forecast*'.ljust(forecast_width)} | {'Change'.strip()}"
+                result.append(header)
+                divider = "-" * len(header)
+                result.append(divider)
 
-  return {"statusCode": status_code}
+            database, forecast, change = row[1:]
+            if not database.endswith("(GB)"):
+                forecast = f"${forecast}"
+            else:
+                forecast = str(forecast)
+            str_forecast = str(forecast).ljust(forecast_width)
+            result.append(f"{database.ljust(account_width)} | {str_forecast} | {change.strip()}")
+
+        secret = get_secret(SECRET_MANAGER, REGION)
+        result.append("* Forecast components may be ±15-25%")
+
+        slack_url = json.loads(secret)[SLACK_SECRET_KEY_NAME]
+        send_slack_message(slack_url, '\n'.join(result))
+
+        teams_url = json.loads(secret)[TEAMS_SECRET_KEY_NAME]
+        send_teams_message(teams_url, '\n'.join(result))
+
+    except Exception as err:
+        status_code = 400
+
+    return {"statusCode": status_code}
--- usage_monitor.sql
+++ usage_monitor.sql
@@ -1,59 +1,10 @@
-/*
-Licensed to the Apache Software Foundation (ASF) under one
-or more contributor license agreements.  See the NOTICE file
-distributed with this work for additional information
-regarding copyright ownership.  The ASF licenses this file
-to you under the Apache License, Version 2.0 (the
-"License"); you may not use this file except in compliance
-with the License.  You may obtain a copy of the License at
-
-  http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing,
-software distributed under the License is distributed on an
-"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-KIND, either express or implied.  See the License for the
-specific language governing permissions and limitations
-under the License.
-*/
-
 CREATE DATABASE IF NOT EXISTS USAGE_MONITOR;
 
 USE DATABASE USAGE_MONITOR;
 
-CREATE OR REPLACE PROCEDURE CALC_METERING_HISTORY_TREND() 
-RETURNS VARCHAR
-LANGUAGE javascript
-as
-$$
-/*
-    Checks the Organization Usage chart for Service level. Sets Multipler to the Cost Per Credit value.
-    Hardcoded to $2/credit if the Service_Level is Standard, and $3.7/credit otherwise.
-*/
-var multiplier = 0;
-var result_set1 = snowflake.execute( { sqlText:
-       `SELECT SERVICE_LEVEL 
-        FROM SNOWFLAKE.ORGANIZATION_USAGE.USAGE_IN_CURRENCY_DAILY 
-        ORDER BY USAGE_DATE 
-        DESC LIMIT 1;`
-        } );
-
-if(result_set1.next()){
-    var serviceLevel = result_set1.getColumnValue(1);
-    
-    if (serviceLevel !== 'Standard') {
-        multiplier = 3.7;
-    } else {
-        multiplier= 2;
-    }
-}
-
-var who_cares = snowflake.execute( { sqlText:
-        `CREATE OR REPLACE TEMPORARY TABLE METERING_HISTORY_TEMPTB(START_TIME_MONTH int, CREDITS_USED double, FORECAST double );`
-       } );
+CREATE OR REPLACE TEMPORARY TABLE METERING_HISTORY_TEMPTB(START_TIME_MONTH int, CREDITS_USED double, FORECAST double );
        
-var who_cares = snowflake.execute( { sqlText:
-        `INSERT INTO METERING_HISTORY_TEMPTB
+INSERT INTO METERING_HISTORY_TEMPTB
             SELECT Top 2 START_TIME_MONTH, CREDITS_USED, FORECAST FROM
               (
                 SELECT MONTH(START_TIME) + YEAR(START_TIME)*100 START_TIME_MONTH,
@@ -76,23 +27,16 @@ var who_cares = snowflake.execute( { sqlText:
                 UNION ALL
                     SELECT 10990101, 0.01, 0.01, 0.01, 0.1
                 )
-            ORDER BY 1;`
-       } );
+            ORDER BY 1;
 
 
-/* ---------------------------------------------------------------- */
-/* -- Create metrics for each Warehouse                          -- */
-/* ---------------------------------------------------------------- */
-var who_cares = snowflake.execute( { sqlText:
-      `CREATE OR REPLACE TEMPORARY TABLE METERING_HISTORY_TREND(ACCOUNT VARCHAR(25), "MTD" VARCHAR(25), "FORECAST" VARCHAR(25), "PRIOR_MONTH" VARCHAR(25), "CHANGE" VARCHAR(25) );`
-       } );
-
-var function1 = snowflake.execute( { sqlText:
-      `INSERT INTO METERING_HISTORY_TREND
-       SELECT ACCOUNT, TO_CHAR(SUM(CREDITS_USED) * ` + multiplier + `,'999,999.00') "MTD", 
-                TO_CHAR(SUM(FORECAST) * ` + multiplier + `,'999,999.00') "FORECAST", 
-                TO_CHAR(SUM(PRIOR_MONTH) * ` + multiplier + `,'999,999.00') "PRIOR_MONTH", 
-                TO_CHAR((SUM(FORECAST) * ` + multiplier + ` - SUM(PRIOR_MONTH) * ` + multiplier + `)/SUM(PRIOR_MONTH) * ` + multiplier + ` * 100, '999,999.0"%"') "CHANGE"
+CREATE OR REPLACE TEMPORARY TABLE METERING_HISTORY_TREND(ACCOUNT VARCHAR(25), "MTD" VARCHAR(25), "FORECAST" VARCHAR(25), "PRIOR_MONTH" VARCHAR(25), "CHANGE" VARCHAR(25) );
+       
+INSERT INTO METERING_HISTORY_TREND
+       SELECT ACCOUNT, TO_CHAR(SUM(CREDITS_USED),'999,999.00') "MTD", 
+                TO_CHAR(SUM(FORECAST),'999,999.00') "FORECAST", 
+                TO_CHAR(SUM(PRIOR_MONTH),'999,999.00') "PRIOR_MONTH", 
+                TO_CHAR((SUM(FORECAST) - SUM(PRIOR_MONTH))/SUM(PRIOR_MONTH)*100, '99,999.0"%"') "CHANGE"
         FROM (
           SELECT CURRENT_ACCOUNT() as ACCOUNT, CREDITS_USED, 0 PRIOR_MONTH, 0 "CHANGE", FORECAST FROM METERING_HISTORY_TEMPTB
           WHERE START_TIME_MONTH = (SELECT MAX(START_TIME_MONTH) FROM METERING_HISTORY_TEMPTB)
@@ -100,13 +44,9 @@ var function1 = snowflake.execute( { sqlText:
           SELECT CURRENT_ACCOUNT(), 0, CREDITS_USED, 0, 0 FROM METERING_HISTORY_TEMPTB
           WHERE START_TIME_MONTH = (SELECT MIN(START_TIME_MONTH) FROM METERING_HISTORY_TEMPTB)
         )
-        GROUP BY 1;`
-       } );
+        GROUP BY 1;
 
-
-/* -- Add Storage tracking in GB -- */
-var who_cares = snowflake.execute( { sqlText:
-      `INSERT INTO METERING_HISTORY_TREND
+INSERT INTO METERING_HISTORY_TREND
           SELECT 'DATABASE_BYTES(GB)' ACCOUNT, TO_CHAR(SUM(MTD/1073741824),'999,999,999.000') "MTD", 
                 TO_CHAR(SUM(FORECAST/1073741824),'999,999,999.000') "FORECAST", 
                 TO_CHAR(SUM(PRIOR_MONTH/1073741824),'999,999,999.000') "PRIOR_MONTH", 
@@ -128,22 +68,14 @@ var who_cares = snowflake.execute( { sqlText:
               WHERE DATE_PART('YEAR',USAGE_DATE) = DATE_PART('YEAR', dateadd(day, -30, getdate())) and DATE_PART('MONTH',USAGE_DATE) = DATE_PART('MONTH', dateadd(day, -30, getdate()))
               )
           )
-          GROUP BY 1;`
-       } );
+          GROUP BY 1;
        
-  var who_cares = snowflake.execute( { sqlText:
-       `DROP TABLE METERING_HISTORY_TEMPTB`
-       } );
+DROP TABLE METERING_HISTORY_TEMPTB
 
-/* ---------------------------------------------------------------- */
-/* -- Create metrics for each Warehouse                          -- */
-/* ---------------------------------------------------------------- */
 
-var who_cares = snowflake.execute( { sqlText:
-        `CREATE OR REPLACE TEMPORARY TABLE METERING_HISTORY_NAME_TEMPTB(NAME VARCHAR(25), START_TIME_MONTH int, CREDITS_USED double, FORECAST double );`
-       } );
-var who_cares = snowflake.execute( { sqlText:
-        `INSERT INTO METERING_HISTORY_NAME_TEMPTB
+CREATE OR REPLACE TEMPORARY TABLE METERING_HISTORY_NAME_TEMPTB(NAME VARCHAR(25), START_TIME_MONTH int, CREDITS_USED double, FORECAST double );
+
+INSERT INTO METERING_HISTORY_NAME_TEMPTB
             SELECT NAME, START_TIME_MONTH, CREDITS_USED, FORECAST FROM
               (
                 SELECT NAME, MONTH(START_TIME) + YEAR(START_TIME)*100 START_TIME_MONTH,
@@ -165,54 +97,27 @@ var who_cares = snowflake.execute( { sqlText:
                 GROUP BY 1,2
                 HAVING MONTH(START_TIME) + YEAR(START_TIME)*100 = MAX(MONTH(START_TIME) + YEAR(START_TIME)*100)
                 )
-            ORDER BY FORECAST DESC;`
-       } );
+            ORDER BY MTD DESC;
        
-var who_cares = snowflake.execute( { sqlText:
-      `CREATE OR REPLACE TEMPORARY TABLE METERING_HISTORY_NAME_TREND(NAME VARCHAR(25), "MTD" VARCHAR(25), "FORECAST" VARCHAR(25), "PRIOR_MONTH" VARCHAR(25), "CHANGE" VARCHAR(25) );`
-       } );
+CREATE OR REPLACE TEMPORARY TABLE METERING_HISTORY_NAME_TREND(NAME VARCHAR(25), "MTD" VARCHAR(25), "FORECAST" VARCHAR(25), "PRIOR_MONTH" VARCHAR(25), "CHANGE" VARCHAR(25) );
 
-var function2 = snowflake.execute( { sqlText:
-      `INSERT INTO metering_history_name_trend
-SELECT 'Snowflake Usage' NAME
-	,To_char(mtd.usage_in_currency, '999,999.00') "MTD"
-	,To_char((
-			(
-				SELECT Sum(usage_in_currency)
-				FROM snowflake.organization_usage.usage_in_currency_daily
-				WHERE usage_date > Dateadd(day, - 30, CURRENT_TIMESTAMP())
-				) / 30
-			) * (Extract(day FROM Last_day(CURRENT_TIMESTAMP())) - Extract(day FROM CURRENT_TIMESTAMP())) + mtd.usage_in_currency, '999,999.00') "FORECAST"
-	,To_char((
-			SELECT Sum(usage_in_currency)
-			FROM snowflake.organization_usage.usage_in_currency_daily
-			WHERE usage_date > Dateadd(month, - 2, CURRENT_TIMESTAMP())
-				AND usage_date < Dateadd(month, - 1, CURRENT_TIMESTAMP())
-			), '999,999.00') "PRIOR_MONTH"
-	,To_char((mtd.usage_in_currency - (
-				SELECT Sum(usage_in_currency)
-				FROM snowflake.organization_usage.usage_in_currency_daily
-				WHERE usage_date > Dateadd(month, - 2, CURRENT_TIMESTAMP())
-					AND usage_date < Dateadd(month, - 1, CURRENT_TIMESTAMP())
-				)) / (
-				SELECT Sum(usage_in_currency)
-				FROM snowflake.organization_usage.usage_in_currency_daily
-				WHERE usage_date > Dateadd(month, - 2, CURRENT_TIMESTAMP())
-					AND usage_date < Dateadd(month, - 1, CURRENT_TIMESTAMP())
-				) * 100, '999,999.0"%"') "CHANGE"
-FROM (
-	SELECT Round(Sum(usage_in_currency), 2) AS usage_in_currency
-	FROM snowflake.organization_usage.usage_in_currency_daily
-	WHERE usage_date >= Trunc(CURRENT_TIMESTAMP(), 'MONTH')
-		AND usage_date <= CURRENT_TIMESTAMP()
-	) mtd
+-- Failed here. Divide by 0
+INSERT INTO metering_history_name_trend
+SELECT 'Snowflake Usage'                        NAME,
+       To_char(usage_in_currency, '999,999.00') "MTD",
+       To_char(usage_in_currency, '999,999.00') "FORECAST",
+       NULL                                     "PRIOR_MONTH",
+       NULL                                     "CHANGE"
+FROM   (SELECT Round(Sum(usage_in_currency), 2) AS usage_in_currency
+        FROM   snowflake.organization_usage.usage_in_currency_daily
+        WHERE  usage_date > Dateadd(month, -1, CURRENT_TIMESTAMP()))
 UNION ALL
 SELECT NAME,
-       To_char(Sum(credits_used)* ` + multiplier + `, '999,999.00') "MTD",
-       To_char(Sum(forecast)* ` + multiplier + `, '999,999.00')     "FORECAST",
-       To_char(Sum(prior_month)* ` + multiplier + `, '999,999.00')  "PRIOR_MONTH",
-       To_char(( Sum(forecast)* ` + multiplier + ` - Sum(prior_month)* ` + multiplier + ` ) / Sum(prior_month)* ` + multiplier + ` * 100,
-       '999,999.0"%"')                             "CHANGE"
+       To_char(Sum(credits_used), '999,999.00') "MTD",
+       To_char(Sum(forecast), '999,999.00')     "FORECAST",
+       To_char(Sum(prior_month), '999,999.00')  "PRIOR_MONTH",
+       To_char(( Sum(forecast) - Sum(prior_month) ) / Sum(prior_month) * 100,
+       '99,999.0"%"')                           "CHANGE"
 FROM   (SELECT NAME,
                credits_used,
                0 PRIOR_MONTH,
@@ -234,10 +139,10 @@ GROUP  BY 1
 HAVING Sum(prior_month) != 0
 UNION ALL
 SELECT NAME,
-       To_char(Sum(credits_used)* ` + multiplier + `, '999,999.00') "MTD",
-       To_char(Sum(forecast)* ` + multiplier + `, '999,999.00')     "FORECAST",
-       To_char(Sum(prior_month)* ` + multiplier + `, '999,999.00')  "PRIOR_MONTH",
-       To_char(Sum(forecast)* ` + multiplier + `, '999,999.0"%"')    "CHANGE"
+       To_char(Sum(credits_used), '999,999.00') "MTD",
+       To_char(Sum(forecast), '999,999.00')     "FORECAST",
+       To_char(Sum(prior_month), '999,999.00')  "PRIOR_MONTH",
+       To_char(Sum(forecast), '99,999.0"%"')    "CHANGE"
 FROM   (SELECT NAME,
                credits_used,
                0 PRIOR_MONTH,
@@ -257,12 +162,10 @@ FROM   (SELECT NAME,
                                    FROM   metering_history_name_temptb))
 GROUP  BY 1
 HAVING Sum(prior_month) = 0
-ORDER  BY forecast DESC;`
-       } );
+ORDER  BY forecast DESC;  
 
-/* -- Add Storage tracking in GB -- */
-    var who_cares = snowflake.execute( { sqlText:
-      `INSERT INTO METERING_HISTORY_NAME_TREND
+
+INSERT INTO METERING_HISTORY_NAME_TREND
           SELECT 'DATABASE_BYTES(GB)' ACCOUNT, TO_CHAR(SUM(MTD/1073741824),'999,999,999.000') "MTD", 
                 TO_CHAR(SUM(FORECAST/1073741824),'999,999,999.000') "FORECAST", 
                 TO_CHAR(SUM(PRIOR_MONTH/1073741824),'999,999,999.000') "PRIOR_MONTH", 
@@ -284,12 +187,7 @@ ORDER  BY forecast DESC;`
               WHERE DATE_PART('YEAR',USAGE_DATE) = DATE_PART('YEAR', dateadd(day, -30, getdate())) and DATE_PART('MONTH',USAGE_DATE) = DATE_PART('MONTH', dateadd(day, -30, getdate()))
               )
           )
-          GROUP BY 1;`
-       } );
+          GROUP BY 1;
        
-  var who_cares = snowflake.execute( { sqlText:
-       `DROP TABLE METERING_HISTORY_NAME_TEMPTB`
-       } );
-       
-  return 'Done.';
-$$;
+DROP TABLE METERING_HISTORY_NAME_TEMPTB
+
